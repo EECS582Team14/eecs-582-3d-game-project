358ce219804938141d6a6d16d316c582ccce11e3
willbhedges <willbhedges@icloud.com>
2026-03-13 16:34:39 -0500

    added an elevator call button model with animation

--- scenes/levels/main_assets/call_button.gd
+++ scenes/levels/main_assets/call_button.gd
@@ -2,11 +2,12 @@ extends StaticBody3D
 
 # Drag the elevator's AnimatableBody3D node into this slot in the Inspector.
 @export var elevator: AnimatableBody3D
-@export var button_mesh: MeshInstance3D
+@export var button_mesh: Node3D
 # Check this box if this button is on the UPPER floor. Leave unchecked for the lower floor.
 @export var is_upper_floor: bool = false
 
 var _waiting: bool = false
+var _anim_player: AnimationPlayer
 
 func activate() -> void:
 	print("CallButton activate() called!")
@@ -19,32 +20,44 @@ func activate() -> void:
 
 	if elevator.is_at_floor(floor_name):
 		return
-	_set_color(Color.GREEN)
+	_play_button_animation()
 	_waiting = true
 	# call_to_floor handles the network broadcast
 	elevator.call_to_floor(floor_name)
 
 func _ready() -> void:
 	NetworkManager.elevator_used.connect(_on_elevator_used)
+	# Find AnimationPlayer in the button model
+	if button_mesh:
+		_anim_player = _find_animation_player(button_mesh)
+
+func _find_animation_player(node: Node) -> AnimationPlayer:
+	for child in node.get_children():
+		if child is AnimationPlayer:
+			return child
+		var result = _find_animation_player(child)
+		if result:
+			return result
+	return null
+
+func _play_button_animation() -> void:
+	if _anim_player == null:
+		return
+	var anims = _anim_player.get_animation_list()
+	if anims.size() > 0:
+		_anim_player.stop()
+		_anim_player.play(anims[0])
 
 func _process(_delta: float) -> void:
 	if not _waiting or elevator == null:
 		return
 	var floor_name = "upper" if is_upper_floor else "lower"
 	if elevator.is_at_floor(floor_name):
-		_set_color(Color.RED)
 		_waiting = false
-
-func _set_color(color: Color) -> void:
-	if button_mesh == null:
-		return
-	var mat = button_mesh.get_active_material(0)
-	if mat:
-		mat.albedo_color = color
 
 func _on_elevator_used(_action: String, floor_name: String):
 	# Update call button color when elevator is called remotely
 	var my_floor = "upper" if is_upper_floor else "lower"
 	if floor_name == my_floor:
-		_set_color(Color.GREEN)
+		_play_button_animation()
 		_waiting = true
