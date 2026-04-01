2700e90c49ca8e5c866e322316a41c01f6a63667
Eric Loseke <143624230+eloseke@users.noreply.github.com>
2026-04-01 12:11:35 -0500

    Elevator networking

--- scenes/levels/main_assets/elevator_button.gd
+++ scenes/levels/main_assets/elevator_button.gd
@@ -2,10 +2,6 @@ extends StaticBody3D
 
 @export var elevator: AnimatableBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 func activate() -> void:
 	print("ElevatorButton activate() called!")
 	
@@ -14,7 +10,3 @@ func activate() -> void:
 		return
 	
 	elevator.activate()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
